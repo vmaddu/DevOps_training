96ce22b97f9b30cbf0ba501ab98ae509ea6ef614
vmaddu <maddukurivramarao@gmail.com>
2018-07-16 16:45:53 +0530

    division method

--- mydir/subtraction.cpp
+++ mydir/subtraction.cpp
@@ -2,12 +2,18 @@
 
 int sub(int,int);
 int add(int,int);
+int div(int,int);
 
 void main()
 {
 	cout<<"Hello World...!";
 	cout<<"subtraction:"<<sub(a,b);
+	cout<<"division:"<<div(a,b);
+}
 
+int div(int a,int b)
+{
+	return a/b;
 }
 
 int sub(int a,int b)
