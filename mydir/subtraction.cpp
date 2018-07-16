5bee9d176add61503ecf808dbd9e1d147e7c2f26
vmaddu <maddukurivramarao@gmail.com>
2018-07-16 16:49:42 +0530

    multiplication method

--- mydir/subtraction.cpp
+++ mydir/subtraction.cpp
@@ -3,12 +3,13 @@
 int sub(int,int);
 int add(int,int);
 int div(int,int);
-
+int mul(int,int);
 void main()
 {
 	cout<<"Hello World...!";
 	cout<<"subtraction:"<<sub(a,b);
 	cout<<"division:"<<div(a,b);
+	cout<<"Multiplication:"<<mul(a,b);
 }
 
 int div(int a,int b)
